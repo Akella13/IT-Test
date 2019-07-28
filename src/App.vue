a78be2bf3b28152dccb3a4dc7b3db7b27fb047db
Arman <akella_piter@mail.ru>
2019-07-28 14:34:59 +0600

    feat: search init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
+    <header class="header">
+      <section>
+        <button type="button">create</button>
+        <button type="button">refresh</button>
+      </section>
+      <search></search>
+    </header>
     <Calendar/>
   </div>
 </template>
 
 <script>
-import Calendar from './components/Calendar.vue'
+import Calendar from './components/Calendar.vue';
+import Search from './components/Search.vue';
 
 export default {
   name: 'app',
   components: {
     Calendar,
+    Search,
   }
 }
 </script>
 
 <style lang="scss">
-
+  body {
+    margin: 0;
+  }
+  .header {
+    background-color: #ccc;
+    width: 100%;
+  }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" v-model="query">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      query: '',
+    }
+  }
+}
+</script>
