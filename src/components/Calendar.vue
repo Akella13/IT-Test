8f99513f0cc28fbd2cb6071c9a02267a4fd1656d
Arman <akella_piter@mail.ru>
2019-07-28 14:29:08 +0600

    feat: Calendar init

---
+++ src/components/Calendar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  data() {
+    return {
+      year: '1900',
+      month: 'Jan',
+      day: 'Sunday',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.notifyOnErrors = false;
 
 new Vue({
   el: '#app',
