1a84b1d917aed8f96f049564c6e116196ccab9bb
Arman <akella_piter@mail.ru>
2019-07-28 16:31:46 +0600

    feat: week

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    
+    <section>
+      <button>prev</button>
+      <span>{{ currentMonth }} {{ currentYear }}</span>
+      <button>next</button>
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div v-for="(day, index) in days" :key="index" class="col day">{{ day }}</div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -9,14 +20,21 @@ export default {
   name: 'Calendar',
   data() {
     return {
-      year: '1900',
-      month: 'Jan',
-      day: 'Sunday',
+      currentYear: '1900',
+      currentMonth: 'Jan',
+      currentDay: 'Sunday',
+      years: [],
+      months: ['Feb', 'Jan', 'March', 'April', 'May', 'June', 'Jule', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+  .day {
+    border: 1px solid #ccc;
+    text-align: center;
+    padding: 1rem;
+  }
 </style>
