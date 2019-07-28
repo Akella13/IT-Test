4fd6ebab8e3cd2fd5ee39ffc683e483054742d72
Arman <akella_piter@mail.ru>
2019-07-28 17:15:59 +0600

    feat: today

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -7,8 +7,10 @@
     </section>
     <section>
       <div class="container">
-        <div class="row">
-          <div v-for="(day, index) in days" :key="index" class="col day">{{ day }}</div>
+        <div v-for="(week, weekIndex) in weeks" :key="weekIndex" class="row">
+          <div v-for="(day, dayIndex) in days" :key="dayIndex" class="col day">
+            <span v-if="week == 1">{{ day }}</span>
+          </div>
         </div>
       </div>
     </section>
@@ -20,13 +22,27 @@ export default {
   name: 'Calendar',
   data() {
     return {
-      currentYear: '1900',
-      currentMonth: 'Jan',
-      currentDay: 'Sunday',
+      today: new Date(),
       years: [],
       months: ['Feb', 'Jan', 'March', 'April', 'May', 'June', 'Jule', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    }
+      weeks: 5,
+      days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      date: 31,
+    };
+  },
+  computed: {
+    currentYear() { return this.today.getFullYear() },
+    currentMonth() { return this.today.getMonth() },
+    currentDay() { return this.today.getDay() },
+    currentDate() { return this.today.getDate() },
+  },
+  mounted() {
+    console.log(this.monthLength(this.currentMonth, this.currentYear))
+  },
+  methods: {
+    monthLength (month, year) {
+      return new Date(year, month, 0).getDate() + 1;
+    },
   }
 }
 </script>
