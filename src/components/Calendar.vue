546bf2da448fd8423e915680f69802edd575815a
Arman <akella_piter@mail.ru>
2019-07-28 21:21:59 +0600

    feat: prevMonth() & nextMonth()

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <section>
-      <button type="button" @click="prethisonth">prev</button>
-      <span>{{ months[selected] }} {{ current.year }}</span>
+      <button type="button" @click="prevMonth">prev</button>
+      <span>{{ months[selected.month] }} {{ selected.year }}</span>
       <button type="button" @click="nextMonth">next</button>
     </section>
     <section>
@@ -31,7 +31,7 @@ export default {
       months: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'Jule', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       weeks: 5,
       days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      selected: 0,
+      selected: {},
       current: {},
     };
   },
@@ -42,7 +42,8 @@ export default {
       day: this.today.getDay(),
       date: this.today.getDate(),
       week: Math.floor(this.today.getDate()/this.days.length),
-    })
+    });
+    this.$set(this, 'selected', this.current);
   },
   mounted() {
     
@@ -56,13 +57,23 @@ export default {
       if (date > 0 && date <= this.monthInDates(this.current.month, this.current.year)) {
         return date;
       }
-      return 'other';
+      return '-';
     },
-    prethisonth() {
-      this.selected -= 1;
+    prevMonth() {
+      if (this.selected.month <= 0) {
+        this.selected.year -= 1;
+        this.selected.month = 11;
+      } else {
+        this.selected.month -= 1;
+      }
     },
     nextMonth() {
-      this.selected += 1;
+      if (this.selected.month >=11) {
+        this.selected.year += 1;
+        this.selected.month = 0;
+      } else {
+        this.selected.month += 1;
+      }
     }
   }
 }
